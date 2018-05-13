6e9fbcd0d822c0f8f9ec26ad98a5cb6d0fd66602
yimo <1280160831@qq.com>
2018-05-13 09:15:25 +0800

    create-branch-v2

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-    uploadApi:'/static/upload.json',
-    editorUploadApi:'/static/editorUpload.json',
-    defPageSize:5,
-    routeMode:'hash',//'history',
-    apiAddress:'http://api.yimo.link'
-
-}
